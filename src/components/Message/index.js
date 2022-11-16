1b81b47a800eac1044c01dc206bd3df5b2a2275f
Mohammedchandpasha <116291572+Mohammedchandpasha@users.noreply.github.com>
2022-11-16 17:11:32 +0530

    Update index.js

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -2,7 +2,7 @@
 
 const Message = props => {
   const {isLoggedIn} = props
-  const message = isLoggedIn ? 'Welcome User' : 'Please Lo'
+  const message = isLoggedIn ? 'Welcome User' : 'Please Login'
   return <h1>{message}</h1>
 }
 export default Message
